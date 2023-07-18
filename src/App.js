ede698e3ec621052eea231d6e2bd3b3ae312bf10
Alexis Sayago <as.sayagom@gmail.com>
2023-07-18 17:25:26 -0400

    first visual settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className='py-2 d-flex justify-content-around card-bg'>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className='py-2 d-flex justify-content-around card-bg'>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
